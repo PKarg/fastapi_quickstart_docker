4b870e473c58399cf2907b6bdc02ad9bc8d07e82
PKarg <patryk.kargulewicz@gmail.com>
2023-12-23 23:30:38 +0100

    initial setup - new setup

---
+++ environment/config.py
@@ -0,0 +1,5 @@
+from pydantic_settings import BaseSettings
+# TODO implement general config using pydantic BaseSettings class that will include
+#  - database connection string
+#  - api server info
+#  - logging settings
