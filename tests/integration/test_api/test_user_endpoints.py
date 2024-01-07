5cfcb443043a78ac5f700c9bb055dcbe6bb0dba3
PKarg <patryk.kargulewicz@gmail.com>
2024-01-07 16:30:47 +0100

    added missing project settings in tests for docs access

--- tests/integration/test_api/test_user_endpoints.py
+++ tests/integration/test_api/test_user_endpoints.py
@@ -2,13 +2,16 @@ from requests.auth import HTTPBasicAuth
 from sqlalchemy import select
 
 from models.users import User
+from run.config import project_settings
 
 
 def test_create_player_endpoint(get_test_db, prepare_test_client, create_test_user):
     db = get_test_db
     client = prepare_test_client
     players = db.execute(select(User)).scalars().all()
-    response = client.post(url="/users", auth=HTTPBasicAuth(username="testadmin", password="testadmin"),
+    response = client.post(url="/users",
+                           auth=HTTPBasicAuth(username=project_settings.main.docs_username,
+                                              password=project_settings.main.docs_password.get_secret_value()),
                            json={"username": "user123",
                                  "raw_password": "pass134567",
                                  "email_address": "test_user@test.com"})
